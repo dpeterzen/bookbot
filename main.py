23528cadc66d1f31dd1359ddd4751ae5a0bae7ca
Dan Peterson <dmpeterson317@gmail.com>
2024-02-19 21:18:55 -0500

    first pass at main file

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = count_words(text)
+    print(num_words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def count_words(text):
+    words = len(text.split())
+    return words
+
+
+main()
